7f396eb4341ad2f7a9f3d0c6378b368c2a1a3878
Vamsi Akkala <vamsi.a@zelarsoft.com>
2021-05-23 17:43:21 +0530

    groovy

--- seed.groovy
+++ seed.groovy
@@ -38,3 +38,113 @@ folder('CI-Pipelines') {
         }
     }
 //}
+
+
+//def component = ["cart", "catalogue","user","payment","shipping","frontend"];
+
+//def count=(component.size()-1)
+//for (i in 0..count) {
+//    def j=component[i]
+pipelineJob('CI-Pipelines/users-ci') {
+    configure { flowdefinition ->
+//            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+//                'triggers' {
+        //                   'hudson.triggers.SCMTrigger' {
+//                        'spec'('*/2 * * * 1-5')
+//                        'ignorePostCommitHooks'(false)
+//                    }
+//                }
+//            }
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/dev0ps01/users.git')
+                        // 'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+//}
+
+//def component = ["cart", "catalogue","user","payment","shipping","frontend"];
+
+//def count=(component.size()-1)
+//for (i in 0..count) {
+//    def j=component[i]
+pipelineJob('CI-Pipelines/login-ci') {
+    configure { flowdefinition ->
+//            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+//                'triggers' {
+        //                   'hudson.triggers.SCMTrigger' {
+//                        'spec'('*/2 * * * 1-5')
+//                        'ignorePostCommitHooks'(false)
+//                    }
+//                }
+//            }
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/dev0ps01/login.git')
+                        // 'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+//}
+
+
+//def component = ["cart", "catalogue","user","payment","shipping","frontend"];
+
+//def count=(component.size()-1)
+//for (i in 0..count) {
+//    def j=component[i]
+pipelineJob('CI-Pipelines/todo-ci') {
+    configure { flowdefinition ->
+//            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+//                'triggers' {
+        //                   'hudson.triggers.SCMTrigger' {
+//                        'spec'('*/2 * * * 1-5')
+//                        'ignorePostCommitHooks'(false)
+//                    }
+//                }
+//            }
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/dev0ps01/todo.git')
+                        // 'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+//}
