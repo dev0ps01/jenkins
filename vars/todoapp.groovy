3b7eb05f1337859c1d523cf98e12684610936e22
Vamsi Akkala <vamsi.a@zelarsoft.com>
2021-05-27 10:32:34 +0530

    groovy

--- vars/todoapp.groovy
+++ vars/todoapp.groovy
@@ -61,9 +61,7 @@
             }
 
             stage('Prepare Artifacts') {
-                when {
-                    environment name: 'APP_TYPE' , value : 'NGINX'
-                }
+
                 steps {
                     script {
                         prepare = new nexus()
